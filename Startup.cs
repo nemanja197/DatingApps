0739d3b341fe4b49428f9a18d399104467b63347
SIMCON-VM\Creen <Milo Stankovi>
2022-07-11 21:21:32 +0200

    promena

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,7 @@ namespace DatingApps
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
